20f82dffa9c9e3ba34cb2fd5b659780c17edc855
LJC <3296473514@qq.com>
2026-07-15 16:59:36 +0800

    feat: 添加日志功能及 MyBatis SQL 日志配置
    - HelloController 添加 @Slf4j 日志，记录接口调用和 API_KEY
    - application.yaml 配置 mapper 包 DEBUG 级别日志

--- demo/src/main/java/com/Myself/demo/controller/HelloController.java
+++ demo/src/main/java/com/Myself/demo/controller/HelloController.java
@@ -2,24 +2,33 @@ package com.Myself.demo.controller;
 
 import com.Myself.demo.entity.TUser;
 import com.Myself.demo.mapper.TUserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.annotation.Resource;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class HelloController {
+
+    private static final String API_KEY = System.getenv("A_BAILIAN_API_KEY");
 
     @Resource
     private TUserMapper tUserMapper;
 
     @GetMapping("/hello")
     public String hello(){
+        log.info("访问 /hello 接口");
+        log.info("API_KEY: {}", API_KEY);
         return "SpringBoot项目运行正常！";
     }
 
     @GetMapping("/user/list")
     public List<TUser> userList(){
-        return tUserMapper.findAll();
+        log.info("访问 /user/list 接口");
+        List<TUser> list = tUserMapper.findAll();
+        log.info("查询到 {} 条用户记录", list.size());
+        return list;
     }
 }
